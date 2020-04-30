c28a03e26b4d3bfa9c13fcb13d1cf2fd889530ef
Miguel Angel <miguelapb35@gmail.com>
2020-04-29 22:36:28 -0500

    Updates

---
+++ precache-manifest.ca927a26e601c500d3ef605f585d01a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e710c6b6c51ef79b88a982284463e7",
+    "url": "/react-redux-shopping-cart/index.html"
+  },
+  {
+    "revision": "becbbb3e98cf5710827a",
+    "url": "/react-redux-shopping-cart/static/css/main.b457e5a0.chunk.css"
+  },
+  {
+    "revision": "9403526f95b7e282919c",
+    "url": "/react-redux-shopping-cart/static/js/2.77c766cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-shopping-cart/static/js/2.77c766cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "becbbb3e98cf5710827a",
+    "url": "/react-redux-shopping-cart/static/js/main.1d786432.chunk.js"
+  },
+  {
+    "revision": "a556ccbe77cbbc5b8df6",
+    "url": "/react-redux-shopping-cart/static/js/runtime-main.2ca711ac.js"
+  }
+]);
